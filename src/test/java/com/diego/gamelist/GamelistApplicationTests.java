d70712158ae3e4891b2bc3bb625a3ccb6556c3f4
Diego <arrudadiego1@gmail.com>
2025-05-20 12:56:14 -0300

    Project creation

---
+++ src/test/java/com/diego/gamelist/GamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.gamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
